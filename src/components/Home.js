37e6331544a44b54ceab6289558b0bc720af2f27
Omar <oabuhammoud@gmail.com>
2024-07-21 23:35:23 -0400

    Updated the Home page, added a typing effect and made my name change colors

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import Typical from 'react-typical';
+import './Home.css';
 
 const Home = () => {
   return (
     <section id="home">
-      <h2>Welcome to my portfolio!</h2>
-      <p>I'm Omar Abuhammoud, a Computer Science student at Cornell University. Explore my projects and learn more about me.</p>
+      <h2>
+        Hi, I'm <span className="color-change">Omar </span>
+      </h2>
+      <p>
+        I'm a{' '}
+        <Typical
+          loop={Infinity}
+          wrapper="span"
+          steps={[
+            'full stack developer',
+            2000,
+            'software developer',
+            2000,
+          ]}
+          className="typing-effect"
+        />
+      </p>
     </section>
   );
 };
